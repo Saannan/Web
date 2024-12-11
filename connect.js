f04c856779c7bc9c876c3bb72c172bef7f7c3a5f
Saannan <sanjayaadiputra376@gmail.com>
2024-12-11 13:45:19 +0700

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/api/ai/vai', require('./api/vai'))
+app.use('/api/dl/aio', require('./api/aio'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
