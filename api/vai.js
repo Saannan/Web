dc0d7f4de5f05b86653a64e662706df92551d160
Saannan <sanjayaadiputra376@gmail.com>
2024-12-11 15:24:23 +0700

    Add files via upload

--- api/vai.js
+++ api/vai.js
@@ -12,7 +12,7 @@ app.get('/api/ai/vai', async (req, res) => {
     const prompt = 'Nama kamu Vioo yang diciptakan oleh Sanjaya dengan ke pintaran yang sangat menakjubkan.';
     const response = await axios.get(`https://api.siputzx.my.id/api/ai/yanzgpt?query=${encodeURIComponent(q)}&prompt=${encodeURIComponent(prompt)}&modelType=yanzgpt-revolution-25b-v3.0`);
 
-    if (response.data?.choices?.[0]?.message?.content) {
+    if (response.data.data.choices[0].message.content) {
       return res.json({
         status: true,
         creator: 'Sanjaya',
