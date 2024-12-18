24e1a9288b8238806517efa569b1fe4b8d104f3a
Saannan <sanjayaadiputra376@gmail.com>
2024-12-18 08:13:09 +0700

    Add files via upload

--- connect.js
+++ connect.js
@@ -6,30 +6,21 @@ const app = express();
 const PORT = 3000;
 app.use(express.static('./public'));
 
-async function drawTextOnTemplate(templatePath, name, text, align) {
+async function drawTextOnTemplate(templatePath, text, align) {
   const imageBuffer = fs.readFileSync(templatePath);
   const image = await loadImage(imageBuffer);
-
   const canvas = createCanvas(image.width, image.height);
   const ctx = canvas.getContext('2d');
-
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-
   ctx.font = '20px Arial';
   ctx.fillStyle = 'black';
   ctx.textAlign = align;
-
-  const nameX = align === 'right' ? 600 : 100;
-  ctx.fillText(name, nameX, 50);
-
   const startX = align === 'right' ? 650 : 120;
   const startY = 150;
   const lineHeight = 35;
-
   const words = text.split(' ');
   let line = '';
   let y = startY;
-
   words.forEach((word) => {
     const testLine = line + word + ' ';
     const testWidth = ctx.measureText(testLine).width;
@@ -41,43 +32,41 @@ async function drawTextOnTemplate(templatePath, name, text, align) {
       line = testLine;
     }
   });
-
   ctx.fillText(line, startX, y);
-
   return canvas.toBuffer('image/png');
 }
 
 app.get('/nuliskanan', async (req, res) => {
-  const { name, text } = req.query;
-  if (!name || !text) {
-    return res.status(400).json({ message: 'Parameter "name" dan "text" harus disertakan!' });
+  const { text } = req.query;
+  if (!text) {
+    return res.status(400).json({ message: 'Parameter "text" harus disertakan!' });
   }
 
   const templatePath = './image/template_kanan.jpg';
   try {
-    const buffer = await drawTextOnTemplate(templatePath, name, text, 'right');
+    const buffer = await drawTextOnTemplate(templatePath, text, 'right');
     res.set('Content-Type', 'image/png');
     res.send(buffer);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error dalam memproses gambar');
+    res.status(500).send('Error memproses gambar');
   }
 });
 
 app.get('/nuliskiri', async (req, res) => {
-  const { name, text } = req.query;
-  if (!name || !text) {
-    return res.status(400).json({ message: 'Parameter "name" dan "text" harus disertakan!' });
+  const { text } = req.query;
+  if (!text) {
+    return res.status(400).json({ message: 'Parameter "text" harus disertakan!' });
   }
 
   const templatePath = './image/template_kiri.jpg';
   try {
-    const buffer = await drawTextOnTemplate(templatePath, name, text, 'left');
+    const buffer = await drawTextOnTemplate(templatePath, text, 'left');
     res.set('Content-Type', 'image/png');
     res.send(buffer);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error dalam memproses gambar');
+    res.status(500).send('Error memproses gambar');
   }
 });
 
