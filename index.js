cc4bc7b6f94dca374d018fa76056db34b8069d44
Saannan <sanjayaadiputra376@gmail.com>
2024-12-18 18:51:31 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = 3000;
 app.use(express.static(path.join(__dirname)));
 
+function Enc(type) {
+return encodeURIComponent(type)
+}
+
 app.get("/api/ai/openai", async (req, res) => {
   const { q } = req.query;
   if (!q) {
@@ -12,13 +16,47 @@ app.get("/api/ai/openai", async (req, res) => {
   }
 
   try {
-    const response = await axios.get(`https://deliriussapi-oficial.vercel.app/ia/chatgpt?q=${q}`);
+    const response = await axios.get(`https://deliriussapi-oficial.vercel.app/ia/chatgpt?q=${Enc(q)}`);
     res.status(200).json({
     status: true,
     result: response.data.data
     })
   } catch (error) {
-    res.status(500).json({ status: false, error: "Failed to fetch ChatGPT" })
+    res.status(500).json({ status: false, error: "Failed" })
+  }
+})
+
+app.get("/api/ai/luminai", async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Text is required" });
+  }
+
+  try {
+    const response = await axios.get(`https://api.siputzx.my.id/api/ai/llama?prompt=You%20name%20is%LuminAI&message=${Enc(q)}`);
+    res.status(200).json({
+    status: true,
+    result: response.data.data
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: "Failed" })
+  }
+})
+
+app.get("/api/ai/llama", async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Text is required" });
+  }
+
+  try {
+    const response = await axios.get(`https://api.siputzx.my.id/api/ai/luminai?content=${Enc(q)}`);
+    res.status(200).json({
+    status: true,
+    result: response.data.data
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: "Failed" })
   }
 })
 
@@ -29,7 +67,7 @@ app.get("/api/src/google", async (req, res) => {
   }
 
   try {
-    const response = await axios.get(`https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(q)}&key=AIzaSyAajE2Y-Kgl8bjPyFvHQ-PgRUSMWgBEsSk&cx=e5c2be9c3f94c4bbb`);
+    const response = await axios.get(`https://www.googleapis.com/customsearch/v1?q=${Enc(q)}&key=AIzaSyAajE2Y-Kgl8bjPyFvHQ-PgRUSMWgBEsSk&cx=e5c2be9c3f94c4bbb`);
     const items = response.data.items;
     res.status(200).json({
     status: true,
@@ -40,7 +78,7 @@ app.get("/api/src/google", async (req, res) => {
     })),
     })
   } catch (error) {
-    res.status(500).json({ status: false, error: "Failed to fetch Google" })
+    res.status(500).json({ status: false, error: "Failed" })
   }
 })
 
