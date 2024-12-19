f5d8595a7ef95e7f95d9aee24b3c7ba19cc4edb7
Saannan <sanjayaadiputra376@gmail.com>
2024-12-20 01:34:48 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,71 @@ function Enc(type) {
   return encodeURIComponent(type)
 }
 
-// ===== AI
+// ===== A
+
+app.get("/api/gpt4o", async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" });
+  }
+  try {
+    const response = await ChatGPT(`${Enc(q)}`, "gpt-4o")
+    res.status(200).json({
+    status: true,
+    result: response
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+})
+
+app.get("/api/gpt4o-mini", async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" });
+  }
+  try {
+    const response = await ChatGPT(`${Enc(q)}`, "gpt-4o-mini")
+    res.status(200).json({
+    status: true,
+    result: response
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+})
+
+app.get("/api/gpt-turbo", async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" });
+  }
+  try {
+    const response = await ChatGPT(`${Enc(q)}`, "gpt-3.5-turbo")
+    res.status(200).json({
+    status: true,
+    result: response
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+})
+
+app.get("/api/gpt-turbov2", async (req, res) => {
+  const { q } = req.query;
+  if (!q) {
+    return res.status(400).json({ status: false, error: "Query is required" });
+  }
+  try {
+    const response = await ChatGPT(`${Enc(q)}`, "gpt-3.5-turbo-0125")
+    res.status(200).json({
+    status: true,
+    result: response
+    })
+  } catch (error) {
+    res.status(500).json({ status: false, error: error.message })
+  }
+})
 
 app.get("/api/openai", async (req, res) => {
   const { q } = req.query;
