f92edade5bacd9e8fa5b596e17df77673471a133
Saannan <sanjayaadiputra376@gmail.com>
2024-12-22 21:40:12 +0700

    Add files via upload

--- search/functions.js
+++ search/functions.js
@@ -272,6 +272,117 @@ quality,
 type
 }}
 
+async function igfbdl(link) {
+const { data } = await axios.post(
+'https://yt1s.io/api/ajaxSearch',
+new URLSearchParams({ p: 'home', q: link, w: '', lang: 'en' }), { headers: {
+'User-Agent': 'Postify/1.0.0',
+'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+}, })
+const $ = cheerio.load(data.data);
+return $('a.abutton.is-success.is-fullwidth.btn-premium').map((_, el) => ({
+title: $(el).attr('title'),
+url: $(el).attr('href'),
+})).get();
+}
+
+async function tiktokdl(link, type = 'video') {
+const headers = {
+"authority": "ttsave.app",
+"accept": "application/json, text/plain, */*",
+"origin": "https://ttsave.app",
+"referer": type === 'mp3' ? 'https://ttsave.app/en/mp3' : 'https://ttsave.app/en',
+"user-agent": "Postify/1.0.0",
+};
+const parse = ($) => {
+const uniqueId = $('#unique-id').val();
+const nickname = $('h2.font-extrabold').text();
+const profilePic = $('img.rounded-full').attr('src');
+const username = $('a.font-extrabold.text-blue-400').text();
+const description = $('p.text-gray-600').text();
+const dlink = {
+nowm: $('a.w-full.text-white.font-bold').first().attr('href'),
+wm: $('a.w-full.text-white.font-bold').eq(1).attr('href'),
+audio: $('a[type="audio"]').attr('href'),
+profilePic: $('a[type="profile"]').attr('href'),
+cover: $('a[type="cover"]').attr('href')
+};
+const stats = {
+plays: '',
+likes: '',
+comments: '',
+shares: ''
+};
+$('.flex.flex-row.items-center.justify-center').each((index, element) => {
+const $element = $(element);
+const svgPath = $element.find('svg path').attr('d');
+const value = $element.find('span.text-gray-500').text().trim();
+if (svgPath && svgPath.startsWith('M10 18a8 8 0 100-16')) {
+stats.plays = value;
+} else if (svgPath && svgPath.startsWith('M3.172 5.172a4 4 0 015.656')) {
+stats.likes = value || '0';
+} else if (svgPath && svgPath.startsWith('M18 10c0 3.866-3.582')) {
+stats.comments = value;
+} else if (svgPath && svgPath.startsWith('M17.593 3.322c1.1.128')) {
+stats.shares = value;
+}});
+const songTitle = $('.flex.flex-row.items-center.justify-center.gap-1.mt-5')
+.find('span.text-gray-500')
+.text()
+.trim();
+const slides = $('a[type="slide"]').map((i, el) => ({
+number: i + 1,
+url: $(el).attr('href')
+})).get();
+return {
+uniqueId,
+nickname,
+profilePic,
+username,
+description,
+dlink,
+stats,
+songTitle,
+slides
+}};
+const data = { "query": link, "language_id": "1" };
+const response = await axios.post('https://ttsave.app/download', data, { headers });
+const $ = cheerio.load(response.data);
+const result = parse($);
+if (type === 'video') {
+return {
+type: 'video',
+...result,
+videoInfo: {
+nowm: result.dlink.nowm,
+wm: result.dlink.wm,
+}}} else if (type === 'mp3') {
+return {
+type: 'audio',
+uniqueId: result.uniqueId,
+nickname: result.nickname,
+username: result.username,
+songTitle: result.songTitle,
+description: result.description,
+stats: result.stats,
+audioUrl: result.dlink.audio,
+coverUrl: result.dlink.cover,
+profilePic: result.profilePic
+}} else if (type === 'slide') {
+return {
+type: 'slide',
+uniqueId: result.uniqueId,
+nickname: result.nickname,
+username: result.username,
+description: result.description,
+stats: result.stats,
+songTitle: result.songTitle,
+slides: result.slides,
+profilePic: result.profilePic,
+coverUrl: result.dlink.cover
+}}
+}
+
 async function spotifydl(link) {
 const headers = {
 'authority': 'www.bhandarimilan.info.np',
@@ -388,4 +499,4 @@ config
 return respons.data
 }
 
-module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, spotifys, bingS, bingI, bingV, ytdl, remini, reminiv2, dehaze, bratv2, transcribe }
+module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, spotifys, bingS, bingI, bingV, ytdl, igfbdl, tiktokdl, remini, reminiv2, dehaze, bratv2, transcribe }
