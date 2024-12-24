7f3f2da6f3e7409de30446fcc74e7e68c835b87c
Saannan <sanjayaadiputra376@gmail.com>
2024-12-24 19:32:33 +0700

    Add files via upload

--- search/functions.js
+++ search/functions.js
@@ -306,43 +306,6 @@ url: $(el).attr('href'),
 })).get();
 }
 
-async function igdl(url) {
-const data = `url=${encodeURIComponent(url)}&v=3&lang=en`
-const config = {
-method: 'POST',
-url: 'https://api.downloadgram.org/media',
-headers: {
-'User-Agent': 'Mozilla/5.0 (Android 10; Mobile; rv:131.0) Gecko/131.0 Firefox/131.0',
-'Content-Type': 'application/x-www-form-urlencoded',
-'accept-language': 'id-ID',
-'referer': 'https://downloadgram.org/',
-'origin': 'https://downloadgram.org',
-'sec-fetch-dest': 'empty',
-'sec-fetch-mode': 'cors',
-'sec-fetch-site': 'same-site',
-'priority': 'u=0',
-'te': 'trailers',
-},
-data: data,
-};
-const response = await axios.request(config);
-const $ = cheerio.load(response.data);
-let mediaInfo = {};
-if ($('video').length) {
-mediaInfo.videoUrl = $('video source').attr('src');
-mediaInfo.downloadUrl = $('a[download]').attr('href');
-mediaInfo.posterUrl = $('video').attr('poster');
-} else if ($('img').length) {
-mediaInfo.imageUrl = $('img').attr('src');
-mediaInfo.downloadUrl = $('a[download]').attr('href');
-}
-for (let key in mediaInfo) {
-if (mediaInfo.hasOwnProperty(key)) {
-mediaInfo[key] = mediaInfo[key].replace(/\\\\"/g, '').replace(/\\"/g, '');
-}}
-return mediaInfo;
-}
-
 async function tiktokdl(link, type = 'video') {
 const headers = {
 "authority": "ttsave.app",
@@ -565,4 +528,4 @@ config
 return respons.data
 }
 
-module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, spotifys, bingS, bingI, bingV, srcLyrics, ytdl, igfbdl, igdl, tiktokdl, getLyrics, pastebin, remini, reminiv2, dehaze, bratv2, transcribe }
+module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, spotifys, bingS, bingI, bingV, srcLyrics, ytdl, igfbdl, tiktokdl, getLyrics, pastebin, remini, reminiv2, dehaze, bratv2, transcribe }
