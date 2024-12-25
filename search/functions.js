2b54e0921c449d91aa8d8f38e089156fcb5c8598
Saannan <sanjayaadiputra376@gmail.com>
2024-12-25 09:11:53 +0700

    Add files via upload

--- search/functions.js
+++ search/functions.js
@@ -292,6 +292,47 @@ quality,
 type
 }}
 
+const formatAudio = ['mp3', 'm4a', 'webm', 'acc', 'flac', 'opus', 'ogg', 'wav'];
+const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
+
+async function ytdlv2(url, format) {
+if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
+throw new Error('Format nya gak valid bro.');
+}
+const configDownload = {
+method: 'GET',
+url: `https://p.oceansaver.in/ajax/download.php?format=${format}&url=${encodeURIComponent(url)}&api=dfcb6d76f2f6a9894gjkege8a4ab232222`, headers: {
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}};
+const response = await axios.request(configDownload);
+if (response.data && response.data.success) {
+const { id, title, info } = response.data;
+const { image } = info;
+const downloadUrl = await cekProgress(id);
+return {
+id: id,
+image: image,
+title: title,
+downloadUrl: downloadUrl
+}} else {
+throw new Error('Failed to fetch video details.');
+}}
+
+async function cekProgress(id) {
+const configProgress = {
+method: 'GET',
+url: `https://p.oceansaver.in/ajax/progress.php?id=${id}`,
+headers: {
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}}
+while (true) {
+const response = await axios.request(configProgress);
+if (response.data && response.data.success && response.data.progress === 1000) {
+return response.data.download_url;
+}
+await new Promise(resolve => setTimeout(resolve, 5000));
+}}
+
 async function igfbdl(link) {
 const { data } = await axios.post(
 'https://yt1s.io/api/ajaxSearch',
@@ -528,4 +569,4 @@ config
 return respons.data
 }
 
-module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, spotifys, bingS, bingI, bingV, srcLyrics, ytdl, igfbdl, tiktokdl, getLyrics, pastebin, remini, reminiv2, dehaze, bratv2, transcribe }
+module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, spotifys, bingS, bingI, bingV, srcLyrics, ytdl, ytdlv2, igfbdl, tiktokdl, getLyrics, pastebin, remini, reminiv2, dehaze, bratv2, transcribe }
