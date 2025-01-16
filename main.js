3f8a9ab0e307c2cca08157f51ac9f0af3f8b7470
Saannan <sanjayaadiputra376@gmail.com>
2025-01-16 21:19:53 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -512,10 +512,11 @@ app.get("/api/yts", async (req, res) => {
     return res.status(400).json({ status: false, error: "Query is required" })
   }
   try {
-    const response = await axios.get(`https://api.ryzendesu.vip/api/search/yt?query=${Enc(q)}`)
+    const { ytsearch } = require('./search/functions')
+    const videos = await ytsearch(`${Enc(q)}`)
     res.status(200).json({
-    status: true,
-    data: response.data.videos,
+      status: true,
+      data: videos
     })
   } catch (error) {
     res.status(500).json({ status: false, error: error.message })
