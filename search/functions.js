b5e411ca0c935160fdcd03b12cd9d3d4e0d7234c
Saannan <sanjayaadiputra376@gmail.com>
2025-01-16 22:41:09 +0700

    Add files via upload

--- search/functions.js
+++ search/functions.js
@@ -929,19 +929,4 @@ results.push({ name: 'ipapi.co', error: error.message });
 return results;
 }
 
-async function ssweb(url) {
-    try {
-        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1080 });
-        await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
-        const screenshot = await page.screenshot({ fullPage: true });
-        await browser.close();
-        return screenshot;
-    } catch (err) {
-        console.error('Error:', err);
-        throw err;
-    }
-}
-
-module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, cbaby, text2img, google, apkpure, spotifys, spotifydl, bingS, bingI, bingV, pinterest, srcLyrics, sfilesrc, ytsearch, ytdl, ytdlv2, igfbdl, tiktokdl, terabox, threads, getLyrics, pastebin, sfiledl, remini, reminiv2, dehaze, bratv2, ephoto, transcribe, shortUrlv1, shortUrlv2, shortUrlv3, cekip, ssweb }
+module.exports = { ChatGPT, feloask, meiliai, islamai, veniceai, cbaby, text2img, google, apkpure, spotifys, spotifydl, bingS, bingI, bingV, pinterest, srcLyrics, sfilesrc, ytsearch, ytdl, ytdlv2, igfbdl, tiktokdl, terabox, threads, getLyrics, pastebin, sfiledl, remini, reminiv2, dehaze, bratv2, ephoto, transcribe, shortUrlv1, shortUrlv2, shortUrlv3, cekip }
